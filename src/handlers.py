2600c196ac758cc58a8e653a5e797f5373aa1f94
bashlii-anatolii <tolya.bem@gmail.com>
2026-01-07 20:29:15 +0200

    written handlers

---
+++ src/handlers.py
@@ -0,0 +1,27 @@
+import logging
+
+from telegram import Update
+from telegram.ext import ContextTypes
+from utils import send_image, send_tet, load_message, show_main_menu
+from gpt import ChatGPTService
+from config import CHATGPT_TOKEN
+
+chatgpt_service = ChatGPTService(CHATGPT_TOKEN)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+loger = logging.getLogger(__name__)
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await send_image(update, context, "start")
+    await send_text(update, context, load_message("start"))
+    await show_main_menu(
+        update,
+        context,
+        {
+            "start": "Main menu",
+            "random": "Rfind out a andom fact",
+            "gpt": "Ask ChatGPT",
+            "talk": "Dialogue with a famous person",
+        }
+    )
